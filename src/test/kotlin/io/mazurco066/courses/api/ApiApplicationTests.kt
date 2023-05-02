a8d5a5f2e8a90223eedd9830a861b960c586df20
Gabriel Mazurco <mazurco066@gmail.com>
2023-05-02 15:39:04 -0300

    feat(project): start

---
+++ src/test/kotlin/io/mazurco066/courses/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mazurco066.courses.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
